2e3db4a2e8b513351311688b63858b60f487b2d8
Nyelus101 <cmuonyelu@gmail.com>
2024-01-05 19:38:31 +0100

    Previous button

--- Quiz-App/src/Components/Quiz.jsx
+++ Quiz-App/src/Components/Quiz.jsx
@@ -9,6 +9,15 @@ const { score, setScore, setGameState } = useContext(QuizContext);
   const [currQuestion, setCurrQuestion] = useState(0);
   const [optionChosen, setOptionChosen] = useState("");
 
+
+  const prevQuestion = () => {
+    if (currQuestion === 0) {
+      currQuestion;
+    } else (
+      setCurrQuestion(currQuestion - 1)
+    )
+  };
+
   const nextQuestion = () => {
     if (Questions[currQuestion].answer == optionChosen) {
       setScore(score + 1);
@@ -33,7 +42,8 @@ const { score, setScore, setGameState } = useContext(QuizContext);
         <button onClick={() => setOptionChosen("C")}> { Questions[currQuestion].optionC } </button>
         <button onClick={() => setOptionChosen("D")}> { Questions[currQuestion].optionD } </button>
       </div>
-      
+
+      <button onClick={prevQuestion}>Previous Question</button>  
       {currQuestion == Questions.length - 1 ? (<button onClick={finishQuiz}>Finish Quiz</button>) : (<button onClick={nextQuestion}> Next Question </button>)}
       
     </div>
